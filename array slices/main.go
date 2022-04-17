9096511da025935550c4eaecf608737920331159
abbrevitory <riq@riseup.net>
2022-04-17 16:40:42 -0600

    i learn go lmao

--- array slices/main.go
+++ array slices/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// var fruitArr [2]string
+
+	// assiging values
+
+	// fruitArr[0] = "orange"
+	// fruitArr[1] = "apple"
+
+	// declare and assign
+	// fruitArr := [2]string{"apple", "orange"}
+
+	// fmt.Println(fruitArr)
+	// fmt.Println(fruitArr[1])
+
+	fruitSlice := [2]string{"apple", "orange"}
+
+	fmt.Println(fruitSlice)
+	fmt.Println(len(fruitSlice))
 }
