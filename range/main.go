36483c1923276225f7cbc83db0ec73cb27cbf209
abbrevitory <riq@riseup.net>
2022-04-17 17:33:36 -0600

    i learn go lmao

--- range/main.go
+++ range/main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	ids := []int{543, 213, 768, 123}
+
+	// loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	// no index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+
+	// add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+	
+	// range with map
+	emails := map[string]string{"bob":"bob@gmail.com", "michelle": "michelle@gmail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	for k := range emails {
+		fmt.Println("Name: " + k)
+	}
 }
